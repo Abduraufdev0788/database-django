a734ffafe0e22c85294b2ebfcadbe9b3a665665e
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-11-07 00:08:45 +0500

    tayyor

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from .models import Task
 
-# Create your views here.
+def home_page(request: HttpRequest) -> HttpResponse:
+    if request.method=="GET":
+        return render(request=request, template_name="home_page.html")
+    elif request.method=="POST":
+        form = request.POST
+
+        new_task = Task(
+            name = form.get('name'),
+            description = form.get('description'),
+            statuse = form.get('statuse')
+        )
+        new_task.save()
+
+    tasks = Task.objects.all()
+    return render(request, "home_page.html", {"tasks": tasks})
+        
