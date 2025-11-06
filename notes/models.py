7a53d4ea1e0b7180e4e696e8b4e323990abe28a1
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-11-06 22:54:21 +0500

    data add

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class Task(models.Model):
+    status = {
+        "1": "yomon",
+        "2" : "yaxshi",
+        "3": "alo",
+        "4": "super"
+    }
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True, default="", null=True)
+    statuse = models.CharField(choices=status, default="2")
+
+    def __str__(self):
+        return f" {self.id} : {self.name}"
+
+
 
